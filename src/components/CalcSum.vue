6898eb797669cbfccfaee8a2e322f57f8e30dc39
sighLS <sightlesssf46@gmail.com>
2022-08-28 10:34:47 +0400

    сделал дз2

--- src/components/CalcSum.vue
+++ src/components/CalcSum.vue
@@ -6,9 +6,29 @@
       = {{ sum }}
     </div>
     <div class="keyboard">
-      <button v-for="operation in operations" :key="operation" @click="count2(operation)">
-        {{ operation }}
-      </button>
+      <div class="keyboard__operations">
+        <button v-for="operation in operations" :key="operation" @click="count(operation)">
+          {{ operation }}
+        </button>
+      </div>
+      <div class="keyboard__keys">
+        <div class="checkbox">
+          <input type="checkbox" id="checkbox" v-model="checked">Отображать экранную клавиатуру
+        </div>
+        <label for="checkbox" class="keyboard__screen" v-if="checked">
+          <button v-for="btn in keyboard" :key="btn" @click="input(btn, picked)">{{ btn }}</button>
+        </label>
+        <div class="keyboard__radio">
+          <input type="radio" name="radio" id="one" v-on:change="leverOp1 ()" checked>
+          <label for="one">1ый операнд</label>
+          <input type="radio" name="radio" id="two" v-on:change="leverOp2 ()">
+          <label for="two">2ый операнд</label>
+          <!--          <input type="radio" name="radio"  v-model="picked" :checked="checked">-->
+          <!--          <label for="one">Один</label>-->
+          <!--          <input type="radio" name="radio" v-model="picked">-->
+          <!--          <label for="two">Два</label>-->
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,41 +40,86 @@ export default {
     sum: 0,
     op1: 0,
     op2: 0,
-    operations: ['+', '-', '*', '/', '//', '^']
+    operations: ['+', '-', '*', '/', '//', '^'],
+    keyboard: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'Backspace'],
+    checked: true,
+    picked: true
+
   }),
   methods: {
-    count2 (operation) {
+    count (operation) {
+      const {
+        op1,
+        op2
+      } = this
       switch (operation) {
         case '+':
-          this.sum = this.op1 + this.op2
+          this.sum = op1 + op2
           break
         case '-':
-          this.sum = this.op1 - this.op2
+          this.sum = op1 - op2
           break
         case '*':
-          this.sum = this.op1 * this.op2
+          this.sum = op1 * op2
           break
         case '/':
-          this.sum = this.op1 / this.op2
+          this.sum = op1 / op2
           break
         case '^':
-          this.sum = Math.pow(this.op1, this.op2)
+          this.sum = Math.pow(op1, op2)
           break
         case '//':
-          this.sum = Math.floor(this.op1 / this.op2)
+          this.sum = Math.floor(op1 / op2)
           break
       }
+    },
+    input (btn, pick) {
+      let op
+      pick === true ? op = this.op1 : op = this.op2
+
+      if (btn === 'Backspace' && op !== '') {
+        const arr = +(op.toString().split('').slice(0, -1).join(''))
+        op = arr
+      } else if (op > 0) {
+        op = +(op.toString() + btn)
+      } else {
+        op = btn
+      }
+      pick === true ? this.op1 = op : this.op2 = op
+    },
+    leverOp1 () {
+      this.picked = true
+    },
+    leverOp2 () {
+      this.picked = false
     }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .calc__screen {
   display: flex;
   justify-content: center;
   gap: 10px;
   margin-bottom: 20px;
+}
+
+.keyboard {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  gap: 20px;
+
+  &__screen {
+    @extend .keyboard;
+    flex-direction: row;
+    gap: 5px;
+  }
+
+  &__keys {
+    @extend .keyboard;
+  }
 }
 
 </style>
